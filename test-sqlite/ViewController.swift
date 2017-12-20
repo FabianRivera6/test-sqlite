c89a8c55320c2ff2ad0e5ff885544d70283647be
RIVERA MEJIA FABIAN ISIDRO <rivera_f@mac06-m102.utt.fr>
2017-12-20 11:40:59 +0100

    Initial Commit

---
+++ test-sqlite/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  test-sqlite
+//
+//  Created by RIVERA MEJIA FABIAN ISIDRO on 20/12/2017.
+//  Copyright © 2017 RIVERA MEJIA FABIAN ISIDRO. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
